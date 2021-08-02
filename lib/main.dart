688247c38a6b0ddfb45763f8fd6a71f276815668
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-02 13:21:50 +0100

    New Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,31 +27,120 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: FlutterLogo(
-            size: 25,
+      appBar: AppBar(
+        title: FlutterLogo(
+          size: 25,
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
           ),
-          elevation: 0,
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          leading: IconButton(
+        ),
+        actions: [
+          IconButton(
             onPressed: () {},
             icon: Icon(
-              Icons.arrow_back,
+              Icons.menu,
               color: Colors.grey,
             ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.menu,
-                color: Colors.grey,
+          )
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true, //Later Change this to see what happens
+        children: [
+          Stack(
+            children: [
+              Container(
+                alignment: Alignment(0, -0.4),
+                height: 100,
+                color: Colors.white,
+                child: Text(
+                  'Get Coaching',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
-        body: Center());
+              Container(
+                margin: EdgeInsets.fromLTRB(25, 90, 25, 0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 2,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 25, 5, 5),
+                          child: Text(
+                            'YOU HAVE',
+                            style: GoogleFonts.quicksand(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 40, 5, 25),
+                          child: RichText(
+                            text: TextSpan(
+                              text: '206',
+                              style: GoogleFonts.quicksand(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40,
+                                color: Colors.black,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: ' \u{1f495}',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 125,
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent[100],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Buy More',
+                          style: GoogleFonts.quicksand(
+                              fontWeight: FontWeight.bold, color: Colors.green),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
