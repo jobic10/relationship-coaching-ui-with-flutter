afaa7ff9f3c19f7b05312b8c57668f7c698d1dd3
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-02 15:15:59 +0100

    Grids

--- lib/main.dart
+++ lib/main.dart
@@ -53,8 +53,9 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
+      backgroundColor: Colors.grey.shade100,
       body: ListView(
-        shrinkWrap: true, //Later Change this to see what happens
+        shrinkWrap: true, //TODO: Later Change this to see what happens
         children: [
           Stack(
             children: [
@@ -123,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       height: 50,
                       width: 125,
                       decoration: BoxDecoration(
-                        color: Colors.greenAccent[100],
+                        color: Colors.greenAccent[100]!.withOpacity(.5),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Center(
@@ -138,9 +139,121 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               )
             ],
-          )
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            margin: EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'MY COACHES',
+                  style: GoogleFonts.quicksand(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  'VIEW PAST SESSIONS',
+                  style: GoogleFonts.quicksand(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 2,
+            mainAxisSpacing: 4,
+            shrinkWrap: true, //TODO: Toggle this later
+            children: [
+              _buildCard('Job', 'Available', 1, true,
+                  'https://loremflickr.com/g/320/240/man/all'),
+              _buildCard('Job', 'Available', 2, true,
+                  'https://loremflickr.com/g/320/240/woman/all'),
+              _buildCard('Job', 'Available', 3, true,
+                  'https://loremflickr.com/g/320/240/student/all'),
+              _buildCard('Job', 'Available', 4, true,
+                  'https://loremflickr.com/g/320/240/woman/all'),
+            ],
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildCard(String name, String status, int cardIndex, bool isAvailable,
+      String imgUrl) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 7,
+      child: Column(
+        children: [
+          SizedBox(height: 12),
+          Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                image: NetworkImage(imgUrl),
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            name,
+            style: GoogleFonts.quicksand(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            status,
+            style: GoogleFonts.quicksand(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+              fontSize: 12,
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Expanded(
+            child: Container(
+              width: 175,
+              decoration: BoxDecoration(
+                color: isAvailable ? Colors.green : Colors.grey,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Request',
+                  style: GoogleFonts.quicksand(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      margin: cardIndex.isEven
+          ? EdgeInsets.fromLTRB(10, 0, 25, 10)
+          : EdgeInsets.fromLTRB(25, 0, 5, 10),
     );
   }
 }
